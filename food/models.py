449ba63a9eaba78168ee978447a5151664e9fcff
peter soleeh <petersoleeh@gmail.com>
2017-12-12 18:40:41 +0300

    add tables to the db

--- food/models.py
+++ food/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    food_pic = models.ImageField(upload_to = 'food/',blank = True)
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    rest_img = models.ImageField(upload_to = 'restaurant/',blank = True)
+    name = models.CharField(max_length=200)
+    rest_desc = models.CharField(max_length=200)
+    menu = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    website_url = models.CharField(max_length=500,blank=True)
+    food = models.ForeignKey(Food,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
