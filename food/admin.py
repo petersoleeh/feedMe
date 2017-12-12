c923d845b80dc253068d833f329773e4e4b8fcc9
peter soleeh <petersoleeh@gmail.com>
2017-12-12 18:39:57 +0300

    setup admin

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Food,Restaurant
 
 # Register your models here.
+class RestaurantAdmin(admin.ModelAdmin):
+    fieldsets = [('Description',{'fields': ['name','rest_desc','menu']}),
+                 ('Contact',{'fields':['website_url','phone_number']}),
+                 ('Food',{'fields':['food']}),
+                 ('Pictures',{'fields':['rest_img']}),]
+
+admin.site.register(Restaurant,RestaurantAdmin)
+admin.site.register(Food)
