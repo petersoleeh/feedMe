07a4d8cd79042cb4ef8f8ea397f9644a0cd91bce
peter soleeh <petersoleeh@gmail.com>
2017-12-12 18:41:10 +0300

    display data to the user from the db

--- food/views.py
+++ food/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Food
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    food = Food.objects.all()
+    return render(request,'index.html',{"food":food})
