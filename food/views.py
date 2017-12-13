4c47670044b20ed127c1e09332ebe4744e21836e
peter soleeh <petersoleeh@gmail.com>
2017-12-13 14:22:37 +0300

    db queries

--- food/views.py
+++ food/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food,Restaurant
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     food = Food.objects.all()
     return render(request,'index.html',{"food":food})
+
+
+def restaurant(request,food_id):
+    rest = Restaurant.objects.filter(food_id=food_id)
+    try:
+        food = Food.objects.get(pk=food_id)
+
+    except Food.DoesNotExist:
+        raise Http404("This Item isn't available")
+
+
+    print(rest)
+    return render(request,'all-food/restaurant.html',{"rest":rest})
+
+
+@login_required(login_url='/accounts/register/')
+def restaurant_details(request,restaurant_id):
+    try:
+        rest = Restaurant.objects.get(pk=restaurant_id)
+    except Restaurant.DoesNotExist:
+        raise Http404("That Restaurant Does Not Exist")
+
+    restaurants = Restaurant.objects.filter(id=restaurant_id)
+
+    return render(request,'all-food/restaurant-details.html',{"restaurants":restaurants})
